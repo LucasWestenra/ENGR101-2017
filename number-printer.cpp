471c8ac925bb9635bafe4d09633b82906f516b47
LucasWestenra <ldwestenra42@gmail.com>
2017-04-10 14:01:18 +1200

    Total overhaul of code, detects IR stuff and does stuff

--- number-printer.cpp
+++ number-printer.cpp
@@ -1,7 +1,35 @@
-#include  <stdio.h>
+# include <stdio.h>
+# include <time.h> 
+# include "E101.h" 
 
-int  main (){
-  int  number = 100;
-    printf("Number is: %d.", number );
-  return  0;
+int main (){  
+  init (); 
+  int adc_reading ; 
+  while (true){         
+    int readingTotal = 0;         
+      for (int i = 0; i<5; i++){ 
+      adc_reading = read_analog (0);                     
+      readingTotal = readingTotal + adc_reading;                     
+    }         
+    largestReading = largestReading/5
+    printf ("%d\n", largestReading);         
+    sleep1 (1 ,0);     
+  }
+  if (largestReading > 150){
+     set_motor(1, 100); //  FORWARD   1 = left
+     set_motor(2, -100); // FORWARD   2 = right
+  }
+  else if(largestReading < 150 || largestReading > 100){
+     set_motor(1, -50); //  TURN LEFT
+     set_motor(2, -50); // TURN LEFT
+  }
+  else if(largestReading < 100 || largestReading > 50){
+     set_motor(1, 50);  // TURN RIGHT
+     set_motor(2, 50);  //TURN RIGHT
+  }
+  else{
+     set_motor(1, -25); // REVERSE ~slowly~~~~~~~~~~~~~~~~>>>
+     set_motor(2, 25); //  REVERSE ~slowly~~~~~~~~~~~~~~~~>>>
+  }
+  return 0; 
 }
